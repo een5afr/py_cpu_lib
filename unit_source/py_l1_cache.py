0851d4b0aebbd2d14343a87bcb1c32aad21c4340
Ahmed R <een5afr@googlemail.com>
2018-01-15 19:11:49 +0000

    First Commit!
    N/A

---
+++ unit_source/py_l1_cache.py
@@ -0,0 +1,14 @@
+import sys
+
+class cache_tag_entry():
+  
+
+
+class py_l1_cache():
+  """ Python based model of a simple l1 cache """
+
+  def __init__(self, **l1_config_args):
+    self.size = l1_config_args.get("")
+    self.ways = 
+    self.sets = 
+
